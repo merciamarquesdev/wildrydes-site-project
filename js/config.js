81a3c722e548038d804534a7e800fec42740de41
Mercia <68623425+mercietc@users.noreply.github.com>
2023-03-05 00:48:01 -0300

    project upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_ZTnkM6DLm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ajon8i75e6ns0ipiqsj83pgtn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
+    },
+    api: {
+        invokeUrl: 'https://ugz5o5nmo7.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
